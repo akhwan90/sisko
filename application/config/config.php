437c4f26846a5c82b3251eedf4c221faca37ace1
Nur Akhwan <akhwan90@gmail.com>
2017-05-17 11:22:35 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://".$_SERVER['SERVER_NAME']."/sisko/sisko_upload/";
+$config['base_url']	= "http://".$_SERVER['SERVER_NAME']."/sisko/";
 
 /*
 |--------------------------------------------------------------------------
